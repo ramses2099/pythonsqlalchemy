c00c75e7f8d0eec5fd9f46a7b001e2f32759dfbf
ramses2099 <ramses2099@gmail.com>
2024-07-29 15:33:37 -0400

    add all the changes from today

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     # create database and all table
     Base.metadata.create_all(engine)
 
-    # insert rows
+    # insert user and rol
     # with Session(engine) as session:
     #     user1 = User(
     #         name="spongebob",
@@ -27,16 +27,52 @@ def main():
     #     profile1 = Profile(name = "admin", user=user1)
     #     session.add_all([user1, profile1])
     #     session.commit()
-
+    
+    # insert other user and rol
+    # with Session(engine) as session:
+    #     user2 = User(
+    #         name="ramses",
+    #         fullname="ramses perez"                       
+    #     )
+    #     # insert profile
+    #     profile2 = Profile(name = "Normal", user=user2)
+    #     session.add_all([user2, profile2])
+    #     session.commit()
+    
     # select user where id == 1
     # with Session(engine) as session:
     #     stmt = select(User).where(User.id == 1)
     #     user = session.execute(statement=stmt).scalar_one()
     #     print(Style.GREEN + f"User Name:{user.name} and Profile: {user.profile.name}")
     #     print(Style.WHITE)
+    
+    # update user where id ==2
+    # with Session(engine) as session:
+    #     stmt = select(User).where(User.id == 2)
+    #     user = session.execute(statement=stmt).scalar_one()
+    #     print(Style.GREEN + f"Full Name:{user.fullname}")
+    #     user.fullname = "juan lopez"
+    #     print(Style.GREEN + f"Update Full Name:{user.fullname}")
+    #     print(Style.WHITE)
+    #     session.commit()
+    
+    # delete profile
+    # with Session(engine) as session:
+    #     profile2 = session.get(Profile, 2)
+    #     session.delete(profile2)
+    #     session.commit()
+    
+    # select all users
+    with Session(engine) as session:
+        stmt = select(User)        
+        users = session.execute(statement=stmt)
+                
+        for i, user in enumerate(users):
+            print(f"index : {i} name :{user[0].name} fullname : {user[0].fullname}")
+           
+        # print(Style.WHITE)
+    
 
-    #  TODO: Data Manipulation with the ORM
-
-
+   
 if __name__ == "__main__":
     main()
